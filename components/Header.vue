d44651f70ae05af093d728083844716c514d199d
Cesar Zavala Varon <zvala93@gmail.com>
2022-09-03 16:44:53 -0500

    Added basic template

---
+++ components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <img src="" alt="logo">
+    <nav>
+      <ul>
+        <li>Nosotros</li>
+        <li>Donaciones & Actividades</li>
+        <li>Contacto</li>
+      </ul>
+    </nav>
+  </header>
+</template>
